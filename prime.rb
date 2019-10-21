9373eb548981d30846ab19220c05ec279e6e1fa1
Tavis Weeden <tavisweeden@gmail.com>
2019-10-21 22:55:36 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,12 +1,12 @@
 require 'pry'
 def prime?(int)
  #a prime is a number that is only divisible by 1 and itself
- if int > 1
- range = (2..int-1).to_a
-  binding.pry
-
- range.none? {|num| int % num == 0}
- #binding.pry
+ if int > 2
+  range = (2..int-1).to_a
+  range.none? {|num| int % num == 0}
+ elsif int == 2
+  true
+  
  else
    false
 
