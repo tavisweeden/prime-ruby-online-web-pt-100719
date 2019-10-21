16cce85bf2d969a28961e2adcc7b4bf5a40d774b
Tavis Weeden <tavisweeden@gmail.com>
2019-10-21 22:56:02 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
-# Add  code here!
+require 'pry'
+def prime?(int)
+ #a prime is a number that is only divisible by 1 and itself
+ if int > 2
+  range = (2..int-1).to_a
+  range.none? {|num| int % num == 0}
+ elsif int == 2
+  true
+  
+ else
+   false
+
+ 
+ end
+end
